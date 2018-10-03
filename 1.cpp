6a778d447f4d0fa3d7cb3c14dca65ff6db75e881
Aditya Dube <32643633+errordube@users.noreply.github.com>
2018-10-04 00:27:22 +0530

    LAB-4

--- 1.cpp
+++ 1.cpp
@@ -1,11 +1,85 @@
 #include<iostream>
-#include<stdio.h>
 using namespace std;
+int size;
+class student
+{
+    int roll,sub1,sub2,sub3,total;
+    static int t,hs1,hs2,hs3,r1,r2,r3,rt;
+public:
+    student()
+    {
+        roll = 0;
+        sub1 = sub2 = sub3 = 0;
+    }
+    void getdata()
+    {
+        cout<<"Enter Roll No.: ";
+        cin>>roll;
+        cin>>sub1>>sub2>>sub3;
+        total = sub1+sub2+sub3;
+        if(sub1>hs1)
+        {
+            hs1=sub1;
+            r1=roll;
+        }
+        if(sub2>hs2)
+        {
+            r2=roll;
+            hs2=sub2;
+        }
+        if(sub3>hs3)
+        {
+            r3=roll;
+            hs3=sub3;
+        }
+        if(total>t)
+        {
+            t=total;
+            rt=roll;
+        }
+    }
+    void display()
+    {
+        cout<<roll<<" ="<<total<<endl;
+    }
+    static void highest()
+    {
+        cout<<"The highest marks are: "<<endl;
+        cout<<"Subject 1: "<<hs1<<" By Roll No. "<<r1<<endl;
+        cout<<"Subject 2: "<<hs2<<" By Roll No. "<<r2<<endl;
+        cout<<"Subject 3: "<<hs3<<" By Roll No. "<<r3<<endl;
+    }
+    static void high_total()
+    {
+        cout<<"\n The Highest Total Marks are: "<<endl;
+        cout<<t<<" By Roll No. "<<rt;
+    }
+};
+int student::hs1=0;
+int student::hs2=0;
+int student::hs3=0;
+int student::r1=0;
+int student::r2=0;
+int student::r3=0;
+int student::t=0;
+int student::rt=0;
 int main()
 {
-    int n,i;
-    printf("Enter an integer: ");
-    scanf("%d",&n);
-    for(i=1;i<=n;i++)
-    printf("WELL DONE!\n");
+    student *s;
+    cout<<" How many students you want to enter:";
+    cin>>size;
+    s=new student [size];
+    for(int i=0;i<size;i++)
+    {
+        cout<<"Enter info. of student "<<i+1<<":\n";
+        s[i].getdata();
+    }
+
+   cout<<"\n\nTotal marks obtained by each student :\n";
+   for(int i=0;i<size;i++)
+   {
+       s[i].display();
+   }
+   student::highest();
+   student::high_total();
 }
--- 3.cpp
+++ 3.cpp
@@ -1,30 +1,89 @@
 #include<iostream>
-#include<stdio.h>
 using namespace std;
-void minimax(int array[],int length, int &min, int &max);
+class DB;
+class DM
+{
+    float meter,centi;
+public:
+    void getdata()
+    {
+        cout<<"\nEnter the distance in (meter-centimeter):";
+        cin>>meter>>centi;
+    }
+    void display()
+    {
+        cout<<"\nThe distance is:";
+        cout<<meter<<"meters and"<<centi<<"centimeter";
+
+    }
+    friend void add(DM &, DB &);
+};
+class DB
+{
+    float inch,feet;
+public:
+    void getdata()
+    {
+        cout<<"\nEnter the distance in(feet-inch):";
+        cin>>inch>>feet;
+    }
+    void display()
+    {
+        cout<<"\nThe distance is:";
+        cout<<inch<<"Inches and"<<feet<<"feets";
+    }
+    friend void add(DM &, DB &);
+};
+void add(DM &a, DB &b)
+{
+    int ch;
+    cout<<"\n Press 1 for meter-centi:";
+    cout<<"\n Press 2 for feet-inch:";
+    cout<<"\n Enter your choice:";
+    cin>>ch;
+    if(ch==1)
+    {
+        DM d;
+        int c=(a.meter*100+a.centi+b.feet*30.48+b.inch*2.54);
+  if(c>=100)
+  {
+   d.meter=c/100;
+   d.centi=c%100;
+  }
+  else
+  {
+   d.meter=0;
+   d.centi=c;
+  }
+  d.display();
+ }
+ else
+ {
+  DB d;
+  int i=(a.meter*39.37+a.centi*.3937008+b.feet*12+b.inch);
+  if(i>=12)
+  {
+   d.feet=i/12;
+   d.inch=i%12;
+  }
+  else
+  {
+   d.feet=0;
+   d.inch=i;
+  }
+  d.display();
+ }
+}
 int main()
 {
-   int ar[10],max,min;
-   int s=10;
-   printf("Enter 10 values\n");
-   for(int i=0;i<10;i++)
-   {
-       scanf("%d",&ar[i]);
-   }
-   minimax(ar,s,min,max);
-   printf("max=%d\nmin=%d",max,min);
-   return 0;
+
+ DM a;
+ DB b;
+ a.getdata();
+ b.getdata();
+ add(a,b);
+
+
 }
-void minimax(int array[],int length, int &min, int &max)
-{
-    max=array[0];
-    min=array[0];
-    for(int i=0;i<10;i++)
-    {
-        if(array[i]>max)
-            max=array[i];
-        if(array[i]<min)
-            min=array[i];
-    }
-}
+
 
